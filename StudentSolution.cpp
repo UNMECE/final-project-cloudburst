b40bd41a63f0f95325c78ec71b2a6e98f0e454eb
Otherfile <sunny.suren122@gmail.com>
2025-12-07 15:00:49 -0700

    Work on fixing the problem whenever all values are below water need

--- StudentSolution.cpp
+++ StudentSolution.cpp
@@ -1,5 +1,6 @@
 #include "acequia_manager.h"
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 /*Instructions for this problem:
@@ -138,11 +139,11 @@ int findCanal(std::vector<Canal *> canals, std::string region)
 	return match;
 }
 
-void release(std::vector<Canal *> canals, std::string region)
+void release(std::vector<Canal *> canals, std::string region, double flowRate)
 {
 	int match = findCanal(canals, region);
 	canals[match]->toggleOpen(true);
-	canals[match]->setFlowRate(1);
+	canals[match]->setFlowRate(flowRate);
 	return;
 }
 
@@ -152,6 +153,76 @@ void close(std::vector<Canal *> canals, std::string region)
 	canals[match]->toggleOpen(false);
 }
 
+double findFlowRate(double waterDiff1, double waterDiff2)
+{
+	double flowRate;
+	double waterDifference = abs(waterDiff1-waterDiff2);
+	
+	flowRate = waterDifference/2;
+
+	if(flowRate==0)
+	{
+		flowRate = 0;
+	}
+	else if(flowRate >= 3.6)
+	{
+		flowRate = 1;
+	}
+	else
+	{
+		flowRate = flowRate/3.6;
+	}
+
+	return flowRate;
+}
+
+void floodResponse(vector<Canal *> canals, int region_index, vector<double> water_diff){
+
+	/*  
+	North Diff = 0 
+	South Diff = 1 
+	East Diff = 2
+	*/
+	
+	if(region_index == 0 && water_diff[2] < water_diff[1]) //  East < South
+	{
+		canals[0]->setFlowRate(findFlowRate(water_diff[0], water_diff[1])); // Canal: N to S
+		canals[2]->setFlowRate(0);
+	}
+	else if(region_index == 0 && water_diff[1] < water_diff[2]) // South < East
+	{
+		canals[2]->setFlowRate(findFlowRate(water_diff[0], water_diff[2])); // Canal: N to E
+		canals[0]->setFlowRate(0);
+	}
+
+	if(region_index == 1 && water_diff[0] < water_diff[2]) // North < East
+	{
+		canals[1]->setFlowRate(findFlowRate(water_diff[1], water_diff[2])); // Canal: S to E
+		canals[3]->setFlowRate(0);
+		
+	}
+	else if(region_index == 1 && water_diff[2] < water_diff[0]) // East < North
+	{
+		canals[1]->setFlowRate(findFlowRate(water_diff[1], water_diff[2])); // Canal: S to E
+		canals[3]->setFlowRate(findFlowRate(water_diff[1], water_diff[2])); // Canal: E to N
+	}
+
+	if(region_index == 2 && water_diff[0] < water_diff[1]) // North < South
+	{
+		canals[3]->setFlowRate(findFlowRate(water_diff[2], water_diff[0])); // Canal: E to N
+		canals[0]->setFlowRate(findFlowRate(water_diff[2], water_diff[0])); // Canal: N to S
+	}
+	else if(region_index == 2 && water_diff[1] < water_diff[0]) // South < North
+	{
+		canals[3]->setFlowRate(findFlowRate(water_diff[2], water_diff[0])); // Canal: E to N
+		canals[0]->setFlowRate(0);
+	}
+}
+
+// droughtResponce(): Similar to floodResponce(), but wth each region, checks if it's in drought.
+// If true, check which region has the most water and requests water from that region.
+
+// Need a way to lock in setting made to certain canals so they are not changed again by future functions.
 
 void solveProblems(AcequiaManager& manager)
 {
@@ -160,86 +231,149 @@ void solveProblems(AcequiaManager& manager)
 	auto canals = manager.getCanals();
 	auto regions = manager.getRegions();
 
+	double northNeed = regions[0]->waterNeed;
+	double northCapacity = regions[0]->waterCapacity;
+
+	double southNeed = regions[1]->waterNeed;
+	double southCapacity = regions[1]->waterCapacity;
+
+	double eastNeed = regions[2]->waterNeed;
+	double eastCapacity = regions[2]->waterCapacity;
+
+
+	for (int i = 0; i < 4; i++){
+		canals[i]->toggleOpen(true);
+	}
+
 	while(!manager.isSolved && manager.hour!=manager.SimulationMax)
 	{
 		double northLevel = regions[0]->waterLevel;
-		double northNeed = regions[0]->waterNeed;
-		double northCapacity = regions[0]->waterCapacity;
 
 		double southLevel = regions[1]->waterLevel;
-		double southNeed = regions[1]->waterNeed;
-		double southCapacity = regions[1]->waterCapacity;
-
 
 		double eastLevel = regions[2]->waterLevel;
-		double eastNeed = regions[2]->waterNeed;
-		double eastCapacity = regions[2]->waterCapacity;
 
-		double north_diff = northLevel - northNeed;
-		double east_diff = eastLevel - eastNeed;
-		double south_diff = southLevel - southNeed;
+		/*  North Diff = 0 
+			South Diff = 1 
+			East Diff = 2
+		*/
 		
+		vector<double> water_diff;
+		water_diff.push_back(northLevel - northNeed);
+		water_diff.push_back(southLevel - southNeed);
+		water_diff.push_back(eastLevel - eastNeed);
+
+		// If South has more water than needed, and North AND East need water,
+		// send South's excess water to East
+		if (water_diff[1] >= 3.6 && water_diff[0] < 0 && water_diff[2] < 0) {
+			canals[1]->setFlowRate(1);
+		}
+		else if (water_diff[1] < 3.6 && water_diff[1] > 0 && water_diff[0] < 0 && water_diff[2] < 0) {
+			canals[1]->setFlowRate(water_diff[1] / 3.6); // Canal: S to E
+		}
+		// Else, if South has less water than needed AND North and water to spare,
+		// send North's excess water to South
+		else if (water_diff[1] < 0 && water_diff[0] > 0) {
+			canals[0]->setFlowRate(findFlowRate(water_diff[1], water_diff[0])); // Canal: N to S
+		}
+		// Else, if South's water is at water needed or North has no water to spare,
+		// set flow rates and canals 0 and 1 to 0
+		else {
+			canals[0]->setFlowRate(0);
+			canals[1]->setFlowRate(0);
+		}
+
+		// If North has more water than needed, and East need water,
+		// send North's excess water to East
+		if (water_diff[0]>= 3.6 && water_diff[2] < 0) {
+			canals[2]->setFlowRate(1);
+		}
+		else if (water_diff[0] < 3.6 && water_diff[0] > 0 && water_diff[2] < 0) {
+			canals[2]->setFlowRate(water_diff[0] / 3.6); // N to E
+		}
+		// Else, if North has less water than needed AND East has and water to spare,
+		// send East's excess water to North
+		else if (water_diff[0] < 0 && water_diff[2] > 0) {
+			canals[3]->setFlowRate(findFlowRate(water_diff[1], water_diff[0])); // Canal: N to S
+		}
+		// Else, if North's water is at water needed or East has no water to spare,
+		// set flow rates and canals 2 and 3 to 0
+		else {
+			canals[2]->setFlowRate(0);
+			canals[3]->setFlowRate(0);
+		}
+		
+		// For loop: If region is in drought,
+		// find the region with the most water, regardless of whether it has excess water to spare,
+		// and give water to region in drought.
+		double mostWater = water_diff[0]; 
+		if (water_diff[0] < 0 && water_diff[1] < 0 && water_diff[2] < 0){
+			
+			for (int i = 0; i < 3; i++){
+				if(mostWater < water_diff[i]){
+					mostWater = water_diff[i];
+				}
+			}
+			
+			if (mostWater == water_diff[0]){
+				canals[0]->setFlowRate(1);
+			}
+			else if (mostWater == water_diff[1]){
+				canals[1]->setFlowRate(1);
+			}
+			else if (mostWater == water_diff[2]){
+				canals[3]->setFlowRate(1);	
+			}
+			
+		}
+
+
+		// Flow Rate Debug
+		/*
 		canals[1]->toggleOpen(true);
 		canals[1]->setFlowRate(1);
 
 		cout << northLevel << endl;
 		cout << southLevel << endl;
 		cout << eastLevel << endl;
+		*/
 
-		if(regions[0]->isInDrought == true && east_diff < south_diff)
+		/*
+		if(water_diff[2] < water_diff[1])
 		{
 			
 		}
-		else if(regions[0]->isInDrought == true && south_diff < east_diff)
+		else if(water_diff[1] < water_diff[2])
 		{
 		
 		}
 
-		if(regions[1]->isInDrought == true && north_diff < east_diff)
+		if(water_diff[0] < water_diff[2])
 		{
 
 		}
-		else if(regions[1]->isInDrought == true && east_diff < north_diff)
+		else if(water_diff[2] < water_diff[0])
 		{
 
 		}
 
-		if(regions[2]->isInDrought == true && north_diff < south_diff)
+		if(water_diff[0] < water_diff[1])
 		{
 
 		}
-		else if(regions[2]->isInDrought == true && south_diff < north_diff)
-		{
-		
-		}
-
-
-		if(regions[0]->isFlooded == true && east_diff < south_diff)
-		{
-			
-		}
-		else if(regions[0]-> isFlooded == true && south_diff < east_diff)
+		else if(water_diff[1] < water_diff[0])
 		{
 		
 		}
-
-		if(regions[1]-> isFlooded == true && north_diff < east_diff)
-		{
-
-		}
-		else if(regions[1]-> isFlooded == true && east_diff < north_diff)
-		{
-
-		}
-
-		if(regions[2]-> isFlooded == true && north_diff < south_diff)
-		{
-
-		}
-		else if(regions[2]-> isFlooded == true && south_diff < north_diff)
-		{
+		*/
 		
-		}
+		/*for(int i = 0; i < regions.size(); i++){
+			if(regions[i]->isFlooded){
+				floodResponse(canals, i, water_diff);
+			}
+		}*/
+
+		
 		
 			// Region Difference check for loop
 		/*for (int i = 0; i < regions.size(); ++i){
@@ -253,7 +387,7 @@ void solveProblems(AcequiaManager& manager)
 			}
 			
 		}*/
-		
+		/*
 		if(northLevel <= northNeed){
 			
 		}
@@ -272,7 +406,7 @@ void solveProblems(AcequiaManager& manager)
 			if(regions[i]->isFlooded == true)
 			{
 				//release water from that region by a canal
-				release(canals, regions[i]->name);
+		
 			}
 			else if(regions[i]->isInDrought = true)
 			{
@@ -286,19 +420,13 @@ void solveProblems(AcequiaManager& manager)
 				close(canals, regions[i]->name);
 			}
 		}
-		
+		*/
 		manager.nexthour();
 	}
-}
 
-
-double findFlowRate(double waterLevel, double waterNeed)
-{
-	double flowRate;
-	double waterDifference = abs(waterLevel-waterNeed);
-	
-	if(waterDifference <= 0)
-	{
-		flowRate = 0;
+	for (int i = 0; i < 4; i++){
+		canals[i]->toggleOpen(false);
 	}
 }
+
+
